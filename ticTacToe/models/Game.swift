40246b47dc0cb758775d51cbe7ffe7e1a1ca9206
sarah nys <sarah.nys@evance.be>
2020-06-25 13:58:05 +0200

    added gameState array

--- ticTacToe/models/Game.swift
+++ ticTacToe/models/Game.swift
@@ -7,21 +7,30 @@
 import Foundation
 
 class Game {
-    var playerOneActive: Bool = true
-    var activeGame: Bool = true
+    var activePlayer: Int = 1
+    var gameIsActive: Bool = true
+    var gameState: Array = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     let winningCombo = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
     
     func getImage(_ tag: Int) -> String {
+        setGameState(tag)
         var imageName = ""
-            if (playerOneActive) {
+            if (activePlayer == 1) {
                 imageName = "cross"
+                activePlayer = 2
                  }
                  else {
                imageName = "circle"
+                activePlayer = 1
                  }
-            playerOneActive = !playerOneActive
         return imageName
         }
+    
+    func setGameState(_ tag: Int) {
+        if (gameState[tag-1] == 0 && gameIsActive == true) {
+            gameState[tag-1] = activePlayer
+        }
+    }
         
     }
     
