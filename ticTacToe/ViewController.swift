1c06fe6d5476d10c877de4850b8a82550376bfe9
sarah nys <sarah.nys@evance.be>
2020-06-25 12:11:03 +0200

    setting up gameboard + viewcontroller

--- ticTacToe/ViewController.swift
+++ ticTacToe/ViewController.swift
@@ -7,7 +7,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var playerOne = true
+    
+    @IBAction func action(_ sender: UIButton) {
+        guard !sender.isSelected  else {
+            return
+        }
+        sender.isSelected = true
+        
+        if (playerOne) {
+            sender.setImage(UIImage(named: "cross"), for: .selected)
+        }
+        else {
+            sender.setImage(UIImage(named: "circle"), for: .selected)
+        }
+        
+        playerOne = !playerOne
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
